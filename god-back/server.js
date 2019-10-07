1dc64ef8151da9e17ab9704cca6da3f35703933c
Camilo Concha <camiloconcha@Camilos-MacBook-Pro.local>
2019-10-06 22:55:10 -0500

    Added game logic

--- god-back/server.js
+++ god-back/server.js
@@ -11,13 +11,14 @@ const compression = require("compression");
 const mongoose = require("mongoose");
 //Server
 const clientPath = `${__dirname}/../`;
-const players = require("./src/controllers/player.controller");
 const dbConfig = require("./src/config/database.config.js");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const gameLogic = require("./src/controllers/GameLogic");
 
+let gameLoading = null;
 
 app.use(express.static(clientPath));
 app.use(bodyParser.json());
@@ -30,7 +31,6 @@ app.use(
 
 app.use(compression());
 app.use(morgan("tiny"));
-
 
 mongoose.Promise = global.Promise;
 mongoose
@@ -47,33 +47,26 @@ mongoose
 
 console.log(`ORIGIN ${clientPath}`);
 
-
 // Constants
-const getApiAndEmit = async socket => {
-  try {
-    socket.emit("message", text); // Emitting a new message. It will be consumed by the client
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
-app.get("/", (req, res) => {
-  res.send("Hello world\n");
-});
 
 server.on("error", err => {
   console.error("ERROR", err);
 });
 
-io.on("connection", socket => {
-  console.log("New client connected"),
-    setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => console.log("Client disconnected"));
+io.on("connection", sock => {
+  if (gameLoading) {
+    new gameLogic(gameLoading, sock);
+    gameLoading = null;
+  } else {
+    gameLoading = sock;
+    gameLoading.emit("message", "Waiting");
+  }
+
+  sock.on("message", text => {
+    io.emit("message", text);
+  });
 });
 
-io.on("add-player", function(name) {
-  players.create(name);
-  io.sockets.emit("players", name);
-});
 require("./src/routes/index")(app);
 
 server.listen(port, () => {
--- god-back/src/controllers/gameLogic.js
+++ god-back/src/controllers/gameLogic.js
@@ -1,3 +1,81 @@
-class gameLogic{
-    
+class GameLogic {
+  constructor(player1, player2) {
+    this._players= [player1,player2];
+    this._turns= [null,null];
+
+
+    this._notification('Game Starts!');
+  }
+
+  _sendToPlayer(playerIndex, msg) {
+    this._players[playerIndex].emit('message', msg);
+  }
+
+
+  _notification(message) {
+    this._players.forEach(player => {
+      player.emit('message',message);
+    });
+  }
+
+  _onTurn(playerNumber, turn) {
+    this._turns[playerNumber] = turn;
+    this._sendToPlayer(playerNumber, `Selection is: ${turn}`);
+
+    this._checkGameOver();
+  }
+
+  _checkGameOver() {
+    const turns = this._turns;
+
+    if (turns[0] && turns[1]) {
+      this._sendToPlayers('Game over ' + turns.join(' : '));
+      this._getGameResult();
+      this._turns = [null, null];
+      this._sendToPlayers('Next Round!!!!');
+    }
+  }
+
+
+  _getGameResult() {
+
+    const p0 = this._decodeTurn(this._turns[0]);
+    const p1 = this._decodeTurn(this._turns[1]);
+
+    const diff = (p1 - p0 + 3) % 3;
+
+    switch (distance) {
+      case 0:
+        this._sendToPlayers('Draw!');
+        break;
+
+      case 1:
+        this._sendWinMessage(this._players[0], this._players[1]);
+        break;
+
+      case 2:
+        this._sendWinMessage(this._players[1], this._players[0]);
+        break;
+    }
+  }
+
+  _sendWinMessage(winner, loser) {
+    winner.emit('message', 'You won!');
+    loser.emit('message', 'You lost.');
+  }
+
+  _decodeTurn(turn) {
+    switch (turn) {
+      case 'rock':
+        return 0;
+      case 'scissors':
+        return 1;
+      case 'paper':
+        return 2;
+      default:
+        throw new Error(`Could not decode turn ${turn}`);
+    }
+  }
 }
+
+module.exports = GameLogic;
